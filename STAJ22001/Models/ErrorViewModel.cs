d4d9444390aacc750aa5efa0f505c37fe7b31813
fth_msss <fatih.donmez2008@hotmail.com>
2024-07-23 18:16:54 +0300

    Add project files.

---
+++ STAJ22001/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace STAJ22001.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
