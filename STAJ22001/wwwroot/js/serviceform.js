855c4cf96245d71019374b3f5da7d23ae1536890
fth_msss <fatih.donmez2008@hotmail.com>
2024-07-29 10:46:40 +0300

    test system for data

---
+++ STAJ22001/wwwroot/js/serviceform.js
@@ -0,0 +1,17 @@
+﻿// Example JavaScript to handle form submission
+
+function SubmitButtonClick() {
+
+
+    var formData = new FormData(this);
+    var data = {};
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+
+    // Do something with the data (e.g., log it, send it to a server)
+    console.log(data);
+
+    // For demonstration purposes
+    alert('Form submitted! Check the console for the data.');
+}
